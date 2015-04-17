794e8e2ff618aaf462ea4da95913d8a80d766219
Stanislav <staspereverzev@gmail.com>
2015-04-17 18:04:05 +0300

    Solve Problem 11

---
+++ test/Problem11Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem11Test {
+
+    @Test
+    public void testSolve() {
+        System.out.println(Problem11.solve());
+    }
+}

9564257155a08c75bc158ac458389a50563b4cdd
Stanislav <staspereverzev@gmail.com>
2015-04-22 12:57:35 +0300

    Solve Problem 13

---
+++ test/Problem13Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem13Test {
+
+    @Test
+    public void testSolve() {
+        System.out.println(Problem13.solve());
+    }
+}
